177d6cb52eb1c7ebde87de9b2d7787179b048c6e
Pavandevops357 <pavankalyan.anumula96@gmail.com>
2019-11-22 01:14:53 +0530

    print command with different ways

--- 02-print.sh
+++ 02-print.sh
@@ -27,5 +27,38 @@ echo -e "hello\nBye"
 
 echo -e "Hello\t\t\tBye"
 
+##  some times we need to print the colours to grab the attention
+
+## \e to enable colours
+
+## syntax of colour enabling is  "\e[colmhello"   ==> col is number of colour given below
+
+##  colour                foreground           background
+#   red                        31                  41
+#   green                      32                  42
+#   yellow                     33                  43 
+#   blue                       34                  44
+#   magenta                    35                  45 
+#   cyan                       36                  46 
+
+echo -e "\e[31mredcolour\e[0m"
+echo -e "\e[32myellowcolour\e[0m"
+echo -e "\e[44mbluebackgroundcolour\e[0m"
+
+
+## you need enable the colour and disable colour code is 0(zero) in place of colour
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
